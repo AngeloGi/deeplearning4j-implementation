f7cf95f99d311979a2355114a43d84953c9d610c
Aggelos Giannoudakis <giann.aggelos@gmail.com>
2023-03-11 14:48:45 +0200

    Initial commit

---
+++ src/main/java/com/angelo/deeplearning4jimplementation/Deeplearning4jImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.angelo.deeplearning4jimplementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Deeplearning4jImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Deeplearning4jImplementationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angelo/deeplearning4jimplementation/Deeplearning4jImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelo.deeplearning4jimplementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deeplearning4jImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
